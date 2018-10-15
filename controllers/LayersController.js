3c89dcf71bfcafc99fbaca5d01c83381cc0e4a83
Beto Noronha <beto_noronha@live.com>
2018-10-15 09:06:20 -0300

    update publish function - fix bug (exists layer)

--- controllers/LayersController.js
+++ controllers/LayersController.js
@@ -13,18 +13,10 @@ function x2js(xml) {
     })
 }
 
-function was_layer_already_published_in_workspace_datastore(workspace, datastore, layer){
-    let list_published_layers = this.layers({workspace, datastore})
-
-    for (let i=0; i<list_published_layers.length; i++){
-        let layer_name = list_published_layers["featureTypes"]["featureType"][0]["name"]
-        // if the layer was already published, so return True
-        if (layer_name == layer)
-            return True
-    }
-
-    return False
-}
+// async function was_layer_already_published_in_workspace_datastore(workspace, datastore, layerSld){
+//     let list_published_layers = await this.layers({workspace, datastore})
+//     return list_published_layers["featureTypes"]["featureType"].some(layer => layer.name == layerSld)
+// }
 
 export class LayersController {
 
@@ -67,18 +59,11 @@ export class LayersController {
         return new Promise( (resolve, reject) => {
             let { workspace, datastore, layer, description, projection } = infos
 
-            //Beto: CHAMA A FUNÇÃO THIS.LAYERS({
-            //  workspace,
-            //  datastore
-            // })
-            //verificar se existe a 'layer' na lista de publicadas, caso exista: não faz nada (resolve())
+            this.remove({workspace, datastore, layer}).then( _ => {
+                throw 'err'
 
-            // if the layer was already published, so unpublish it before publising it, because the table structure can change
-            if (was_layer_already_published_in_workspace_datastore(workspace, datastore, layer))
-                this.remove({workspace, datastore, layer})
-
-            // now publish the layer again
-            let bodyXML = `<featureType>
+            }).catch(_ => {
+                let bodyXML = `<featureType>
                 <name>${layer}</name>
                 <nativeName>${layer}</nativeName>
                 <description>${description}</description>
@@ -86,49 +71,51 @@ export class LayersController {
                 <store class="dataStore">
                     <name>${datastore}</name>
                 </store>
-            </featureType>`
+                </featureType>`
 
-            let options = {
-                url: `${environment.geoserver.url}/rest/workspaces/${workspace}/datastores/${datastore}/featuretypes.xml`,
-                method: 'POST',
-                headers: {
-                    "Content-Type": "text/xml",
-                    "Content-Length": bodyXML.length
-                },
-                auth: {
-                    'user': environment.geoserver.user,
-                    'pass': environment.geoserver.password
-                },
-                body: bodyXML.replace(/(\r\n\t|\n|\r\t)/gm,"")
-            }
-
-            request(options, (err, resp, body) => {
-                if(!err && !body) {
-                    resolve()
-
-                } else if(!err){
-                    logger.error(body)
-                    let message
-                    if(resp.statusCode == 400) message = "layer not found"
-                    else message = body
-
-                    reject({
-                        status: 404,
-                        errors: [{
-                            messages: [message]
-                        }]
-                    })
-
-                } else {
-                    logger.error(err)
-                    reject({
-                        status: 500,
-                        errors: [{
-                            messages: ["ERRO: connection with geoserver"]
-                        }]
-                    })
+                let options = {
+                    url: `${environment.geoserver.url}/rest/workspaces/${workspace}/datastores/${datastore}/featuretypes.xml`,
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "text/xml",
+                        "Content-Length": bodyXML.length
+                    },
+                    auth: {
+                        'user': environment.geoserver.user,
+                        'pass': environment.geoserver.password
+                    },
+                    body: bodyXML.replace(/(\r\n\t|\n|\r\t)/gm,"")
                 }
+
+                request(options, (err, resp, body) => {
+                    if(!err && !body) {
+                        resolve()
+
+                    } else if(!err){
+                        logger.error(body)
+                        let message
+                        if(resp.statusCode == 400) message = "layer not found"
+                        else message = body
+
+                        reject({
+                            status: 404,
+                            errors: [{
+                                messages: [message]
+                            }]
+                        })
+
+                    } else {
+                        logger.error(err)
+                        reject({
+                            status: 500,
+                            errors: [{
+                                messages: ["ERRO: connection with geoserver"]
+                            }]
+                        })
+                    }
+                })
             })
+                
         })
     }
 
